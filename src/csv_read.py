00710844fb10698c49697c34735c1d83068ccdd2
Gulyás Gréta <gulyas.greta@yahoo.com>
2021-06-30 15:39:35 +0200

    feat: implemented required CSV read function

---
+++ src/csv_read.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def read_csv_pandas (csv):
+    return pd.read_csv(csv)
---
+++ tests/csv_read_test.py
@@ -0,0 +1,16 @@
+import unittest
+import pandas as pd
+from src.csv_read import read_csv_pandas
+
+class CSVReadDataTets(unittest.TestCase):
+
+    def test_csv(self):
+        # given
+        EXPECTED = True
+        # when
+        ACTUAL = isinstance(read_csv_pandas(), pd.DataFrame)
+        # then
+        self.assertEqual(EXPECTED, ACTUAL)
+
+    # def test_fails(self):
+    #     self.assertEqual(True, False)
